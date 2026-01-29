44d8b3cfc6b4ae173cf5dd909fbdc1c91d779140
Carolina <csolorzanoomar1419@gmail.com>
2026-01-29 13:51:27 +0100

    comentarios

--- Ejercicio1.php
+++ Ejercicio1.php
@@ -1,20 +1,36 @@
 <?php
+//datos fijos 
 if (!isset($_SESSION['numbers'])) {
-    $_SESSION['numbers'] = [10,20,30];
+    //datos de nuestro array
+    $_SESSION['numbers'] = [10, 20, 30];
 }
+//rececpciona el boton presionado por mi usuario 
 if (isset($_POST['modify'])) {
+    //posicion del array escogida por el usuario 
+    //recordar que las posiciones empiezan del 0 en adelante 
+    //es decir que posicion 0 es 10 y posicion 2 es 30 
     $position = $_POST['position'];
+    //valor ingresado por nuestro usuario 
     $value = $_POST['value'];
+    //nos aseguramos de la posicion 
+    //siempre es bueno una confirmacion
     if (isset($_SESSION['numbers'][$position])) {
         $_SESSION['numbers'][$position] = $value;
     }
 }
+//verificacion boton average 
 if (isset($_POST['average'])) {
-    $average = array_sum($_SESSION['numbers']) / count($_SESSION['numbers']);
+    //array_sum= suma nuestros valores del array
+    $average = array_sum($_SESSION['numbers'])
+        //obtenemos cantidad de elementos del array
+        / count($_SESSION['numbers']);
+    //luego calculamos promedio 
 }
 ?>
-    
+
+
 <!DOCTYPE html>
+
 <head>
     <meta charset="UTF-8">
     <title>Modify array saved in session</title>
@@ -22,11 +38,14 @@ if (isset($_POST['average'])) {
 
 <body>
     <h1>Modify array saved in session</h1>
+
     <form action="Ejercicio1.php" method="post">
         <label for="position"> Position to modify</label>
         <select name="position" id="position">
-            <?php for ($i = 0; $i < count(value: $_SESSION['numbers']); $i++) { ?>
-                <option value="<?php echo $i; ?>">
+            <?php
+            for ($i = 0; $i < count(value: $_SESSION['numbers']); $i++) {
+            ?>
+                <option value=" <?php echo $i; ?>">
                     <?php echo $i; ?> </option>
             <?php
             } ?>
@@ -37,19 +56,25 @@ if (isset($_POST['average'])) {
 
         <input type="number" id="value" name="value">
         <br><br>
-
+        /*botones*/
         <input type="submit" value="Modify" name="modify">
         <input type="submit" value="Average" name="average">
         <input type="reset" value="reset">
     </form>
 
-    <p>Current array: 
-        <?php echo implode(', ', $_SESSION['numbers']); ?>
+    <p>Current array:
+        <?php
+        echo implode(
+            ', ',
+            $_SESSION['numbers']
+        ); ?>
     </p>
-    <?php 
+    <?php
     if (isset($average)) {
+        //printa average 
         echo "<p> Average: " . $average . "</p>";
     } ?>
 
 </body>
+
 </html>
